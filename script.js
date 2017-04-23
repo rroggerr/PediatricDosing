c19d9f466b354d102853085a4f8fa5f79d0d4a75
Roger Geng <geng.roger@gmail.com>
2017-04-23 22:24:41 +0800

    first draft
    This reverts commit ca53d536037c4eb76c1a6d2c232d7145f9c9ad0a.
    
    # Conflicts:
    #	index.html
    #	script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var drugnameslist;
 
-function init() {
-	drugnameslist = ["opium", "heroine", "cocaine", "marijuana"];
-	}
+var drugnames = ["opium", "heroine","cocaine","marijuana"];
+
+var input = document.getElementById("drug-search");
+var options=[];
+
+var states = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  // `states` is an array of state names defined in "The Basics"
+  local: drugnames
+});
 
 
-$("#drug-search").typeahead({
-	source:drugnameslist,
-	fitToElement:true
-	});
