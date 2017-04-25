3562bf8e1116f3aa0c14c613a0106fe1539e599c
Roger Geng <geng.roger@gmail.com>
2017-04-25 18:20:26 +0800

    Search by ingredients

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var PNames=[];
+var Ingredients =[];
 var states;
 var unparsed;
 var parsed_json;
@@ -24,7 +25,9 @@ Papa.parse("https://rawgit.com/rroggerr/PharmaRef/master/PD.csv", {
 function processData(p_json){
 	for (var i = 1; i < p_json.length; i++){
 		var tmp = p_json[i].PName;
+		var tmp_ing = p_json[i].Ingredients;
 	    PNames.push(tmp);
+	    Ingredients.push(tmp_ing);
 	}
 }
 
@@ -40,6 +43,13 @@ function init_TA(){
 	  local: PNames
 	});
 
+	ings = new Bloodhound({
+	  datumTokenizer: Bloodhound.tokenizers.whitespace,
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  // `states` is an array of state names defined in "The Basics"
+	  local: Ingredients
+	});
+
 	$('.typeahead').typeahead({
 			  hint: false,
 			  highlight: true,
@@ -47,17 +57,29 @@ function init_TA(){
 			},
 			{
 			  name: 'states',
-			  source: states
-			});
+			  source: states,
+			  templates: {
+						    header: '<h5 class="hname">&nbsp;&nbsp;Product Names: </h5>'
+						  }
+			},
+			{
+			  name: 'ings',
+			  source: ings,
+			  templates: {
+						    header: '<h5 class="hname">&nbsp;&nbsp;Ingredients: </h5>'
+						  }
+
+			}
+			);
 	document.getElementById("drug-search").placeholder="Type in drug name";
 }
 
 function clicksearch(){
 	var textval = $(".typeahead").typeahead("val");
-	document.getElementById("selected-drug-name").innerHTML=textval;
 	var dosagebox =document.getElementById("selected-drug-dosage");
 	for (var i = 0; i < parsed_json.length; i++) {
-		if (parsed_json[i].PName === textval){
+		if (parsed_json[i].PName === textval || parsed_json[i].Ingredients === textval){
+			document.getElementById("selected-drug-name").innerHTML=parsed_json[i].PName;
 			var altdose = parsed_json[i].AltDosage;
 			if (altdose==""){
 				dosagebox.innerHTML = parsed_json[i].DosageChildren;
